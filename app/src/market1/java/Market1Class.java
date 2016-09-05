eaa9173361712bacf40aca75d39ecd56f8b20529
Abhiraj Khare <abhi@Abhirajs-MacBook-Pro.local>
2016-09-05 22:12:25 +0530

    first commit

---
+++ app/src/market1/java/Market1Class.java
@@ -0,0 +1,14 @@
+import com.google.gson.stream.JsonReader;
+
+import org.joda.time.JodaTimePermission;
+
+import xgi.ut.dsl.interpreters.PrettyPrinter;
+
+/**
+ * Created by abhi on 8/22/16.
+ */
+public class Market1Class {
+    PrettyPrinter prettyPrinter;                // Common libs
+    JsonReader gson;                            // Market1
+    JodaTimePermission jodaTimePermission;      // Market2
+}
---
+++ app/src/market2/java/Market2Class.java
@@ -0,0 +1,14 @@
+package market2;
+
+import org.joda.time.JodaTimePermission;
+
+import xgi.ut.dsl.interpreters.PrettyPrinter;
+
+/**
+ * Created by abhi on 8/22/16.
+ */
+public class Market2Class {
+    PrettyPrinter prettyPrinter;                // Common libs
+    com.google.gson.stream.JsonReader gson;     // Market1
+    JodaTimePermission jodaTimePermission;      // Market2
+}
